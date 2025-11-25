457a01af11918324123abfeeedbb3bede689e82d
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-11-25 10:03:33 -0300

    programa de ejemplo

---
+++ clase_2511/ejemplo_cast.py
@@ -0,0 +1,11 @@
+# Programa que pida al usuario ingresar dos numeros y imprimir el resultado de la suma
+
+
+numero1 = int(input("Ingresar un valor para numero1: "))
+numero2 = int(input("Ingresar un valor para numero2: "))
+
+# numero1 = int(numero1)
+# numero2 = int(numero2)
+
+resultado = numero1 + numero2
+print("resultado: ", resultado)
