641071eaf75c750be68e385d515e70f676e5f465
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-11-29 00:07:54 -0300

    material de la clase del 27/11

---
+++ clase_2_2711/prueba_f_strings.py
@@ -0,0 +1,15 @@
+# ejemplo de f strings -- a partir de python 3.7 
+
+
+nombre = "juan"
+edad = 28
+altura = 1.75
+
+
+print(f"Hola, mi nombre es {nombre}, tengo {edad} años y mido {altura} metros.")
+
+frase = f"{nombre} tiene {edad} años y su altura es {altura} metros."
+frase = f"{frase} sigo agregando la frase"
+
+
+print(frase)
