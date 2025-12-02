d567ed452003ad8dc706960b31a3cda39f99a517
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-12-02 15:51:02 -0300

    material de la clse 3 - 03/12

---
+++ clase_3_0212/matematica.py
@@ -0,0 +1,22 @@
+
+# import math
+# 
+# resultado_pow = math.pow(2, 3)
+# resultado_sqrt = math.sqrt(16)
+#
+# print("2 elevado a 3 es:", resultado_pow)
+# print("La raíz cuadrada de 16 es:", resultado_sqrt)
+
+
+# from math import pow, sqrt
+#
+# resultado = pow(2, 3)
+# print("2 elevado a 3 es:", resultado)
+# resultado_sqrt = sqrt(16)
+# print("La raíz cuadrada de 16 es:", resultado_sqrt)
+
+
+from math import *
+
+resultado = pow(2, 3)
+print("2 elevado a 3 es:", resultado)
