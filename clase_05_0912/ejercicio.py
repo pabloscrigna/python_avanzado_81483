0c86d933e55109683867d9f81147a612d835cf10
Pablo Scrigna <pscrigna@gruposimpli.com>
2025-12-12 21:59:17 -0300

    material de la clase 11/12

--- clase_05_0912/ejercicio.py
+++ clase_05_0912/ejercicio.py
@@ -1,30 +1,62 @@
 # dada una frase y un diccionario con con los pesos de las palabras
 # calcular el porcentaje de positividad del texto
-# Buscar las palabras que estan en la frase y calcular el promed segun el peso del diccionario
+# Buscar las palabras que estan en la frase y calcular el promedio segun el peso del diccionario
 # si la palabra no esta en el diccionario el peso es cero
 
-frase = "estoy en un mundo feliz"
 
-peso_total = 5 + 0 + 0 + 0 + 10 = 15
-promedio = 15/5
+
+# frase = "estoy en un mundo feliz"
+# 
+# peso_total = 5 + 0 + 0 + 0 + 10 = 15
+# promedio = 15/5
+# 
+# diccionario_pesos = {
+#     "estoy": 5,
+#     "feliz": 10
+# }
+# 
+# lista_palabras = split de frase
+# 
+# cantidad_palabras = len(lista_palabras)
+# 
+# for palabra in lista_palabras:
+# 
+#     diccionario_pesos[palabra] ??? 
+# 
+#     diccionario_pesos.get(palabra)  -----> None
+#     diccionario_pesos.get(palabra, 0)
+# 
+#     hay que llevar un acumulado
+# 
+# 
+# promedio = acumulado / cantidad_palabras
+# 
+# 
 
 diccionario_pesos = {
     "estoy": 5,
     "feliz": 10
 }
 
-lista_palabras = split de frase
 
-cantidad_palabras = len(lista_palabras)
+def positividad(frase: int) -> float:
 
-for palabra in lista_palabras:
+    acumulador = 0
+    lista_palabras = frase.split()
 
-    diccionario_pesos[palabra] ??? 
+    for palabra in lista_palabras:
+        valor = diccionario_pesos.get(palabra, 0)    # acumulador += diccionario_pesos.get(..)
+        acumulador += valor      # acumulador = acumulador + valor
 
-    diccionario_pesos.get(palabra)  -----> None
-    diccionario_pesos.get(palabra, 0)
+    cantidad_palabras = len(lista_palabras)
+    promedio = acumulador / cantidad_palabras
+    
+    return promedio 
 
-    hay que llevar un acumulado
+
+frase = "estoy en un mundo feliz"
 
 
-promedio = acumulado / cantidad_palabras
+promedio = positividad(frase)
+
+print("promedio", promedio)
